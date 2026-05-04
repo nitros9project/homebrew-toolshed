d1a03ea776f98b29cc6783a7ec1ce63054cb2d66
tim lindner <tlindner@macmess.org>
2026-05-03 19:58:17 -0700

    Update to v2.5.1

--- Formula/ToolShed.rb
+++ Formula/ToolShed.rb
@@ -1,8 +1,8 @@
 class ToolShed < Formula
   desc "Software tools for TRS-80 Color Computer and Dragon micro hobbyists"
   homepage "https://github.com/nitros9project/toolshed"
-  url "https://github.com/nitros9project/toolshed/archive/refs/tags/v2.5.1.tar.gz"
-  sha256 "?????"
+  url "https://codeload.github.com/nitros9project/toolshed/tar.gz/refs/tags/v2.5.1"
+  sha256 "d133c5a1b340f5af7412243b1d0b8168215ee908b79861157e756767b6fe9a34"
   version "2.5.1"
   license "none"
   head "https://github.com/nitros9project/toolshed.git", branch: "main"
